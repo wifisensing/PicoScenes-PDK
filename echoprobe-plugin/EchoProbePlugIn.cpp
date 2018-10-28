8b9d96b76e1b50b206d3c6e5f1db43b1c63dacf6
Zhiping Jiang <jiangzp.cs@gmail.com>
2018-10-28 13:43:22 +0800

    change parameter pll to sf

--- echoprobe-plugin/EchoProbePlugIn.cpp
+++ echoprobe-plugin/EchoProbePlugIn.cpp
@@ -33,7 +33,7 @@ void EchoProbePlugIn::initialization() {
             ("5300", "Both Destination and Source MAC addresses are set to 'magic Intel 00:16:ea:12:34:56'")
 
             ("cf", po::value<std::string>(), "MATLAB-style specification for carrier frequency scan range, format begin:step:end, e.g., 5200e6:20e6:5800e6")
-            ("pll", po::value<std::string>(), "MATLAB-style specification for baseband PLL multipler scan range, format begin:step:end, e.g., 11:11:88")
+            ("sf", po::value<std::string>(), "MATLAB-style specification for baseband sampling frequency multipler scan range, format begin:step:end, e.g., 11:11:88")
             ("repeat", po::value<std::string>(), "The injection number per cf/bw combination, 100 as default")
             ("delay", po::value<std::string>(), "The delay between successive injections(unit in us, 5e5 as default)")
             ("delayed-start", po::value<uint32_t>(), "A one-time delay before injection(unit in second, 0 as default)")
@@ -138,8 +138,8 @@ bool EchoProbePlugIn::handleCommandString(std::string commandString) {
             parameters->cf_end = boost::lexical_cast<double>(rangeParts[2]);
     }
 
-    if (vm.count("pll")) {
-        auto rangeString = vm["pll"].as<std::string>();
+    if (vm.count("sf")) {
+        auto rangeString = vm["sf"].as<std::string>();
         std::vector<std::string> rangeParts;
         boost::split(rangeParts, rangeString, boost::is_any_of(":"), boost::token_compress_on);
         if (!rangeParts[0].empty())
