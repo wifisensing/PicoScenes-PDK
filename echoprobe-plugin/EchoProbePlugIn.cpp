34a9aa292cac32148958efd10ef09956079cc9cd
Zhiping Jiang <zpj@xidian.edu.cn>
2018-12-04 22:52:18 +0800

    disable cf_step check temporarily

--- echoprobe-plugin/EchoProbePlugIn.cpp
+++ echoprobe-plugin/EchoProbePlugIn.cpp
@@ -158,27 +158,29 @@ bool EchoProbePlugIn::handleCommandString(std::string commandString) {
         if (!rangeParts[2].empty())
             parameters->cf_end = boost::lexical_cast<double>(rangeParts[2]);
 
-        if (hal->isAR9300 == false && parameters->cf_begin) {
-            auto closestFreq = closest(hal->systemSupportedFrequencies, *parameters->cf_begin / 1e6);
-            if (*parameters->cf_begin/1e6 != closestFreq) {
-                LoggingService::warning_print("CF begin (currently {}) is forced to be {}MHz for Intel 5300 NIC.\n", *parameters->cf_begin, closestFreq);
-                parameters->cf_begin = (int64_t)closestFreq * 1e6;
+        if (hal->isAR9300 == false) {
+            if (parameters->cf_begin) {
+                auto closestFreq = closest(hal->systemSupportedFrequencies, *parameters->cf_begin / 1e6);
+                if (*parameters->cf_begin/1e6 != closestFreq) {
+                    LoggingService::warning_print("CF begin (currently {}) is forced to be {}MHz for Intel 5300 NIC.\n", *parameters->cf_begin, closestFreq);
+                    parameters->cf_begin = (int64_t)closestFreq * 1e6;
+                }
             }
-        }
 
-        if (hal->isAR9300 == false && (*parameters->cf_step != 0 && std::abs(*parameters->cf_step) != 5e6)) {
-            LoggingService::warning_print("CF step (currently {}Hz) is forced to be +/-5MHz for Intel 5300 NIC.\n", *parameters->cf_step);
-            if (*parameters->cf_step > 0)
-                parameters->cf_step = 5e6;
-            else
-                parameters->cf_step = -5e6;
-        }
-        
-        if (hal->isAR9300 == false && parameters->cf_end) {
-            auto closestFreq = closest(hal->systemSupportedFrequencies, *parameters->cf_end / 1e6);
-            if (*parameters->cf_end/1e6 != closestFreq) {
-                LoggingService::warning_print("CF end (currently {}) is forced to be {}MHz for Intel 5300 NIC.\n", *parameters->cf_end, closestFreq);
-                parameters->cf_end = (int64_t)closestFreq * 1e6;
+//            if (*parameters->cf_step != 0 && std::abs(*parameters->cf_step) != 5e6 && std::abs(*parameters->cf_step) != 20e6) {
+//                LoggingService::warning_print("CF step (currently {}Hz) is forced to be +/- (5 or 20)MHz for Intel 5300 NIC.\n", *parameters->cf_step);
+//                if (*parameters->cf_step > 0)
+//                    parameters->cf_step = 5e6;
+//                else
+//                    parameters->cf_step = -5e6;
+//            }
+
+            if (parameters->cf_end) {
+                auto closestFreq = closest(hal->systemSupportedFrequencies, *parameters->cf_end / 1e6);
+                if (*parameters->cf_end/1e6 != closestFreq) {
+                    LoggingService::warning_print("CF end (currently {}) is forced to be {}MHz for Intel 5300 NIC.\n", *parameters->cf_end, closestFreq);
+                    parameters->cf_end = (int64_t)closestFreq * 1e6;
+                }
             }
         }
     }
