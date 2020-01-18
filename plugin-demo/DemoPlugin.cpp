5bb74b0bd6a0777a48d40972ad0db0e28b54deb2
Zhiping Jiang <zpj@xidian.edu.cn>
2020-01-18 22:36:42 +0800

    demo plugin roughly done.

---
+++ plugin-demo/DemoPlugin.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Zhiping Jiang on 10/20/17.
+//
+
+#include "DemoPlugin.h"
+
+const std::string DemoPlugin::getPluginName() {
+    return "PicoScenes Demo Plugin";
+}
+
+const std::string DemoPlugin::getPluginDescription() {
+    return "Demonstrate the PicoScenes Plugin functionality";
+}
+
+const std::string DemoPlugin::pluginStatus() {
+    return "this method returns the status of the plugin.";
+}
+
+std::shared_ptr<boost::program_options::options_description> DemoPlugin::pluginOptionsDescription() {
+    static auto instance = std::make_shared<boost::program_options::options_description>("Demo Options based on boost::program_options");
+    static std::once_flag onceFlag;
+    std::call_once(onceFlag, [&] {
+        instance->add_options()
+                ("option1", po::value<uint16_t>(), "You should refer Boost::Program_Options for the help on this thing.");
+    });
+
+    return instance;
+}
+
+void DemoPlugin::parseAndExecuteCommands(const std::string &commandString) {
+    po::variables_map vm;
+    auto parsedOptions = po::command_line_parser(po::split_unix(commandString)).options(*pluginOptionsDescription()).allow_unregistered().style(po::command_line_style::unix_style & ~po::command_line_style::allow_guessing).run();
+    po::store(parsedOptions, vm);
+    po::notify(vm);
+
+    if (vm.count("interface")) {
+        auto option1Value = vm["option1"].as<uint32_t>();
+        LoggingService::info_print("Demo Plugin: option1={}.\n", int(option1Value));
+    }
+}
---
+++ plugin-demo/DemoPlugin.h
@@ -0,0 +1,28 @@
+//
+// Created by Zhiping Jiang on 10/20/17.
+//
+
+#ifndef PICOSCENES_TESTPLUGIN_H
+#define PICOSCENES_TESTPLUGIN_H
+
+#include <iostream>
+#include <mutex>
+#include <PicoScenes/AbstractPicoScenesPlugin.hxx>
+#include <PicoScenes/PicoScenesNIC.hxx>
+
+class DemoPlugin : public AbstractPicoScenesPlugin {
+public:
+    const std::string getPluginName() override;
+
+    const std::string getPluginDescription() override;
+
+    const std::string pluginStatus() override;
+
+    std::shared_ptr<boost::program_options::options_description> pluginOptionsDescription() override;
+
+    void parseAndExecuteCommands(const std::string &commandString) override;
+};
+
+PICOSCENES_PLUGIN_INIT(DemoPlugin)
+
+#endif //PICOSCENES_TESTPLUGIN_H
