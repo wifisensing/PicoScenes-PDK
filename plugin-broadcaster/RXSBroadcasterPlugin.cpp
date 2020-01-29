44ac14548f2c98e1facaf4533ada5da675fb2ec4
Zhiping Jiang <jiangzp.cs@gmail.com>
2020-01-29 20:58:35 +0800

    frame broadcaster roughly finished.

---
+++ plugin-broadcaster/RXSBroadcasterPlugin.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Zhiping Jiang on 10/20/17.
+//
+
+#include "RXSBroadcasterPlugin.h"
+
+std::string RXSBroadcasterPlugin::getPluginName() {
+    return "RXS_Broadcaster";
+}
+
+std::string RXSBroadcasterPlugin::getPluginDescription() {
+    return "broadcast all received rxs to network via UDP";
+}
+
+std::shared_ptr<boost::program_options::options_description> RXSBroadcasterPlugin::pluginOptionsDescription() {
+    static auto options = std::make_shared<po::options_description>("Options for plugin " + this->getPluginName());
+    static std::once_flag onceFlag;
+    std::call_once(onceFlag, [&] {
+        options->add_options()
+                ("rxs-broadcaster-dest-ip", po::value<std::string>(), "Destination IP for PlugIn RXS-Broadcaster")
+                ("rxs-broadcaster-dest-port", po::value<uint16_t>(), "Destination Port for PlugIn RXS-Broadcaster");
+    });
+    return options;
+}
+
+std::string RXSBroadcasterPlugin::pluginStatus() {
+    return "Destination IP/Port: " + destinationIP + ":" + std::to_string(destinationPort);
+}
+
+
+void RXSBroadcasterPlugin::parseAndExecuteCommands(const std::string &commandString) {
+    po::variables_map vm;
+    po::store(po::command_line_parser(po::split_unix(commandString)).options(*pluginOptionsDescription().get()).allow_unregistered().run(), vm);
+    po::notify(vm);
+
+    if (vm.count("rxs-broadcaster-dest-ip")) {
+        destinationIP = vm["rxs-broadcaster-dest-ip"].as<std::string>();
+    }
+
+    if (vm.count("rxs-broadcaster-dest-port")) {
+        destinationPort = vm["rxs-broadcaster-dest-port"].as<uint16_t>();
+    }
+}
+
+void RXSBroadcasterPlugin::rxHandle(const PicoScenesRxFrameStructure &rxframe) {
+    UDPService::getInstance("RXSExport" + destinationIP + std::to_string(destinationPort))->sendData(rxframe.rawBuffer.get(), rxframe.rawBufferLength, destinationIP, destinationPort);
+}
---
+++ plugin-broadcaster/RXSBroadcasterPlugin.h
@@ -0,0 +1,33 @@
+//
+// Created by Zhiping Jiang on 10/20/17.
+//
+
+#ifndef PICOSCENES_RXSBROADCASTPLUGIN_H
+#define PICOSCENES_RXSBROADCASTPLUGIN_H
+
+#include <PicoScenes/AbstractPicoScenesPlugin.hxx>
+#include <PicoScenes/UDPService.h>
+
+
+class RXSBroadcasterPlugin : public AbstractPicoScenesPlugin {
+public:
+    std::string getPluginName() override;
+
+    std::string getPluginDescription() override;
+
+    std::string pluginStatus() override;
+
+    std::shared_ptr<boost::program_options::options_description> pluginOptionsDescription() override;
+
+    void parseAndExecuteCommands(const std::string &commandString) override;
+
+    void rxHandle(const PicoScenesRxFrameStructure &rxframe) override;
+
+private:
+    std::string destinationIP = "127.0.0.1";
+    uint16_t destinationPort = 50000;
+};
+
+PICOSCENES_PLUGIN_INIT(RXSBroadcasterPlugin)
+
+#endif //PICOSCENES_RXSBROADCASTPLUGIN_H
