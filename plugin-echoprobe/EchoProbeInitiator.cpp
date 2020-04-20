d04251473303e5f0c9699321145e27471c3bc71e
Zhiping Jiang <jiangzp.cs@gmail.com>
2020-04-20 22:39:54 +0800

    optimize tx parameters

--- plugin-echoprobe/EchoProbeInitiator.cpp
+++ plugin-echoprobe/EchoProbeInitiator.cpp
@@ -154,9 +154,9 @@ std::tuple<std::optional<PicoScenesRxFrameStructure>, std::optional<PicoScenesRx
         auto timeout_us_scaling = nic->getConfiguration()->getSamplingRate() < 20e6 ? 6 : 1;
         auto totalTimeOut = timeout_us_scaling * *parameters.timeout_ms;
         if (!responderDeviceType || responderDeviceType == PicoScenesDeviceType::USRP)
-            totalTimeOut += 200;
+            totalTimeOut += 150;
         if (nic->getDeviceType() == PicoScenesDeviceType::USRP)
-            totalTimeOut += 300;
+            totalTimeOut += 200;
         auto replyFrame = nic->syncRxConditionally([=](const PicoScenesRxFrameStructure &rxframe) -> bool {
             return rxframe.PicoScenesHeader && (rxframe.PicoScenesHeader->frameType == EchoProbeReply || rxframe.PicoScenesHeader->frameType == EchoProbeFreqChangeACK) && rxframe.PicoScenesHeader->taskId == taskId;
         }, std::chrono::milliseconds(totalTimeOut), "taskId[" + std::to_string(taskId) + "]");
@@ -166,7 +166,7 @@ std::tuple<std::optional<PicoScenesRxFrameStructure>, std::optional<PicoScenesRx
             if (replyFrame->PicoScenesHeader->frameType == EchoProbeReply) {
                 auto rxDeviceType = replyFrame->PicoScenesHeader->deviceType;
                 responderDeviceType = rxDeviceType;
-                const auto &segment = replyFrame->segmentMap->at("EP");
+                const auto segment = replyFrame->segmentMap->at("EP"); // using copy to prevent a rare crash case.
                 auto ackFrame = PicoScenesRxFrameStructure::fromBuffer(segment.second.get(), segment.first);
                 if (ackFrame) {
                     if (LoggingService::localDisplayLevel <= Debug) {
