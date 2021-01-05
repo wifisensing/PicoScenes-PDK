07d0bc33512c2456a92fdca346941b7bccd6306d
Zhiping Jiang <zpj@xidian.edu.cn>
2021-01-05 16:12:33 +0800

    update minor

--- Fast_Build_Install_PicoScenes.sh
+++ Fast_Build_Install_PicoScenes.sh
@@ -5,6 +5,6 @@ scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo apt purge picoscenes-plugins-demo-echoprobe-forwarder -y
 
 cd $scriptDir && rm -rf $scriptDir/build && mkdir $scriptDir/build && cd $scriptDir/build
-cmake .. && make package -j6
+cmake .. && make package -j`nproc`
 
 cd $scriptDir/build && sudo dpkg -i ./picoscenes*.deb
