87052aaa2c2e88d0739c72735aa68a7918f53457
Zhiping Jiang <zpj@xidian.edu.cn>
2021-10-04 00:02:49 +0800

    update get_git_timestamp.sh script

--- get_git_timestamp.sh
+++ get_git_timestamp.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ $# == 0 ]]; then
+  targetGitDir="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
+else
+  targetGitDir=$1
+fi
 currentDir="$(pwd)"
 
-cd $scriptDir
-git_timestamp=$(git log -1 --pretty=format:"%ad" --date=format:%y.%m%d.%H%M)
+cd $targetGitDir
+git_timestamp=$(git log -1 --pretty=format:"%ad" --date=format:%Y.%m%d.%H%M)
 cd $currentDir # cd back to current dir to prevent pwd change
 echo $git_timestamp
