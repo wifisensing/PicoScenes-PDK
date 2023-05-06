651c9bdb36189492729d5d415b4af1fff74aefa6
Zhiping Jiang <zpj@xidian.edu.cn>
2023-05-06 16:26:07 +0800

    auto build and installation on macOS

--- Fast_Build_Install_Plugin.sh
+++ Fast_Build_Install_Plugin.sh
@@ -6,13 +6,14 @@ if [ $(uname) = Linux ]; then
     sudo apt purge picoscenes-plugins-demo-echoprobe-forwarder -y
 elif [ $(uname) = Darwin ]; then
     scriptDir=$(cd "$(dirname "$0")"; pwd)
-    ncpu=$(sysctl -n hw.ncpu)
+    ncpu=$(sudo sysctl -n hw.ncpu) # use sudo to acquire privilege earlier
 fi
 
 if [ ! -d $scriptDir/build ]; then
     mkdir $scriptDir/build
 else
     rm -rf $scriptDir/build/*.deb 2>/dev/null
+    rm -rf $scriptDir/build/*.pkg 2>/dev/null
 fi
 
 cd $scriptDir/build
@@ -22,4 +23,6 @@ cpack
 
 if [ $(uname) = Linux ]; then
     cd $scriptDir/build && sudo dpkg -i ./picoscenes*.deb
+elif [ $(uname) = Darwin ]; then
+    cd $scriptDir/build && sudo installer -pkg ./picoscenes*.pkg -target /
 fi
